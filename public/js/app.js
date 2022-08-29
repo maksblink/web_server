2c72e75228a3b9b260b6c3d4802a650610d3904a
maks <blinkmaks777@gmail.com>
2022-08-29 16:47:09 +0200

    to https

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
