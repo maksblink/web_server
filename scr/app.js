453dc42071883a40e7837c9f3ccd8fb64b71a549
maks <blinkmaks777@gmail.com>
2022-08-29 15:47:08 +0200

    setup for heroku

--- scr/app.js
+++ scr/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -64,6 +65,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is runing')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
